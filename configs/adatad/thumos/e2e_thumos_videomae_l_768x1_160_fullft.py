938f45fd97e3401955f368ab262fbaacd98e5a24
shuming <sming256@gmail.com>
2024-04-02 09:25:35 +0300

    confirm thumos-full-ft config

---
+++ configs/adatad/thumos/e2e_thumos_videomae_l_768x1_160_fullft.py
@@ -0,0 +1,11 @@
+_base_ = ["e2e_thumos_videomae_s_768x1_160_fullft.py"]
+
+model = dict(
+    backbone=dict(
+        backbone=dict(embed_dims=1024, depth=24, num_heads=16),
+        custom=dict(pretrain="pretrained/vit-large-p16_videomae-k400-pre_16x4x1_kinetics-400_20221013-229dbb03.pth"),
+    ),
+    projection=dict(in_channels=1024),
+)
+
+work_dir = "exps/thumos/adatad/e2e_actionformer_videomae_l_768x1_160_fullft"
