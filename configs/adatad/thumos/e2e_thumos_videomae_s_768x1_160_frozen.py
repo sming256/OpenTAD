bf2f9bb9dfc43080b8fb3870e1b0a721b2cf6997
shuming <sming256@gmail.com>
2024-04-02 09:25:07 +0300

    confirm thumos-frozen config

---
+++ configs/adatad/thumos/e2e_thumos_videomae_s_768x1_160_frozen.py
@@ -0,0 +1,142 @@
+_base_ = [
+    "../../_base_/datasets/thumos-14/e2e_train_trunc_test_sw_256x224x224.py",  # dataset config
+    "../../_base_/models/actionformer.py",  # model config
+]
+
+window_size = 768
+scale_factor = 1
+chunk_num = window_size * scale_factor // 16  # 768/16=48 chunks, since videomae takes 16 frames as input
+dataset = dict(
+    train=dict(
+        pipeline=[
+            dict(type="PrepareVideoInfo", format="mp4"),
+            dict(type="mmaction.DecordInit", num_threads=4),
+            dict(
+                type="LoadFrames",
+                num_clips=1,
+                method="random_trunc",
+                trunc_len=window_size,
+                trunc_thresh=0.75,
+                crop_ratio=[0.9, 1.0],
+                scale_factor=scale_factor,
+            ),
+            dict(type="mmaction.DecordDecode"),
+            dict(type="mmaction.Resize", scale=(-1, 160)),
+            dict(type="mmaction.CenterCrop", crop_size=160),
+            dict(type="mmaction.FormatShape", input_format="NCTHW"),
+            dict(type="ConvertToTensor", keys=["imgs", "gt_segments", "gt_labels"]),
+            dict(type="Collect", inputs="imgs", keys=["masks", "gt_segments", "gt_labels"]),
+        ],
+    ),
+    val=dict(
+        window_size=window_size,
+        pipeline=[
+            dict(type="PrepareVideoInfo", format="mp4"),
+            dict(type="mmaction.DecordInit", num_threads=4),
+            dict(type="LoadFrames", num_clips=1, method="sliding_window", scale_factor=scale_factor),
+            dict(type="mmaction.DecordDecode"),
+            dict(type="mmaction.Resize", scale=(-1, 160)),
+            dict(type="mmaction.CenterCrop", crop_size=160),
+            dict(type="mmaction.FormatShape", input_format="NCTHW"),
+            dict(type="ConvertToTensor", keys=["imgs", "gt_segments", "gt_labels"]),
+            dict(type="Collect", inputs="imgs", keys=["masks", "gt_segments", "gt_labels"]),
+        ],
+    ),
+    test=dict(
+        window_size=window_size,
+        pipeline=[
+            dict(type="PrepareVideoInfo", format="mp4"),
+            dict(type="mmaction.DecordInit", num_threads=4),
+            dict(type="LoadFrames", num_clips=1, method="sliding_window", scale_factor=scale_factor),
+            dict(type="mmaction.DecordDecode"),
+            dict(type="mmaction.Resize", scale=(-1, 160)),
+            dict(type="mmaction.CenterCrop", crop_size=160),
+            dict(type="mmaction.FormatShape", input_format="NCTHW"),
+            dict(type="ConvertToTensor", keys=["imgs"]),
+            dict(type="Collect", inputs="imgs", keys=["masks"]),
+        ],
+    ),
+)
+
+
+model = dict(
+    backbone=dict(
+        type="mmaction.Recognizer3D",
+        backbone=dict(
+            type="VisionTransformerCP",
+            img_size=224,
+            patch_size=16,
+            embed_dims=384,
+            depth=12,
+            num_heads=6,
+            mlp_ratio=4,
+            qkv_bias=True,
+            num_frames=16,
+            norm_cfg=dict(type="LN", eps=1e-6),
+            return_feat_map=True,
+            with_cp=False,
+        ),
+        data_preprocessor=dict(
+            type="mmaction.ActionDataPreprocessor",
+            mean=[123.675, 116.28, 103.53],
+            std=[58.395, 57.12, 57.375],
+            format_shape="NCTHW",
+        ),
+        custom=dict(
+            pretrain="pretrained/vit-small-p16_videomae-k400-pre_16x4x1_kinetics-400_my.pth",
+            pre_processing_pipeline=[
+                dict(type="Rearrange", keys=["frames"], ops="b n c (t1 t) h w -> (b t1) n c t h w", t1=chunk_num),
+            ],
+            post_processing_pipeline=[
+                dict(type="Reduce", keys=["feats"], ops="b n c t h w -> b c t", reduction="mean"),
+                dict(type="Rearrange", keys=["feats"], ops="(b t1) c t -> b c (t1 t)", t1=chunk_num),
+                dict(type="Interpolate", keys=["feats"], size=window_size),
+            ],
+            norm_eval=True,  # set all norm layers to eval mode, default is True
+            freeze_backbone=True,  # whether to freeze the backbone, default is False
+        ),
+    ),
+    projection=dict(
+        in_channels=384,
+        max_seq_len=window_size,
+        attn_cfg=dict(n_mha_win_size=-1),
+        input_pdrop=0.2,
+    ),
+)
+
+solver = dict(
+    train=dict(batch_size=2, num_workers=2),
+    val=dict(batch_size=2, num_workers=2),
+    test=dict(batch_size=2, num_workers=2),
+    clip_grad_norm=1,
+    amp=True,
+    fp16_compress=True,
+    ema=True,
+)
+
+optimizer = dict(type="AdamW", lr=1e-4, weight_decay=0.05, paramwise=True)
+scheduler = dict(type="LinearWarmupCosineAnnealingLR", warmup_epoch=5, max_epoch=100)
+
+inference = dict(load_from_raw_predictions=False, save_raw_prediction=False)
+post_processing = dict(
+    nms=dict(
+        use_soft_nms=True,
+        sigma=0.5,
+        max_seg_num=2000,
+        min_score=0.001,
+        multiclass=True,
+        voting_thresh=0.7,  #  set 0 to disable
+    ),
+    save_dict=False,
+)
+
+workflow = dict(
+    logging_interval=50,
+    checkpoint_interval=5,
+    val_loss_interval=-1,
+    val_eval_interval=5,
+    val_start_epoch=39,
+    end_epoch=60,
+)
+
+work_dir = "exps/thumos/adatad/e2e_actionformer_videomae_s_768x1_160_frozen"
