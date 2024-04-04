e2c2d08ee8b8f94de8eb0db1d5190c52606dd4ee
shuming <sming256@gmail.com>
2024-04-04 15:12:51 +0300

    update thumos adapter results

---
+++ configs/adatad/thumos/e2e_thumos_videomae_h_768x1_160_adapter.py
@@ -0,0 +1,18 @@
+_base_ = ["e2e_thumos_videomae_s_768x1_160_adapter.py"]
+
+model = dict(
+    backbone=dict(
+        backbone=dict(
+            embed_dims=1280,
+            depth=32,
+            num_heads=16,
+            adapter_index=list(range(32)),
+        ),
+        custom=dict(pretrain="pretrained/vit-huge-p16_videomae-k400-pre_16x4x1_kinetics-400_my.pth"),
+    ),
+    projection=dict(in_channels=1280),
+)
+
+optimizer = dict(backbone=dict(custom=[dict(name="adapter", lr=1e-4, weight_decay=0.05)]))
+
+work_dir = "exps/thumos/adatad/e2e_actionformer_videomae_h_768x1_160_adapter"
