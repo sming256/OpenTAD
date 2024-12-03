5feaec9e77e6ef641cc919cf254dc1ce0494f7cd
Tal Darom <tal.darom@gmail.com>
2024-12-03 18:37:03 +0200

    Update util.py to fix remove duplicate segments bug (#47)
    * Update util.py
    
    Fix a bug where the same segment appears three times, twice with the same label is not removed as a duplicate

--- opentad/datasets/base/util.py
+++ opentad/datasets/base/util.py
@@ -4,16 +4,14 @@ import numpy as np
 def filter_same_annotation(annotation):
     gt_segments = []
     gt_labels = []
+    gt_both = []
     for gt_segment, gt_label in zip(annotation["gt_segments"].tolist(), annotation["gt_labels"].tolist()):
-        if (gt_segment not in gt_segments) or (gt_label not in gt_labels):
+        if (gt_segment, gt_label) not in gt_both:
             gt_segments.append(gt_segment)
             gt_labels.append(gt_label)
+            gt_both.append((gt_segment, gt_label))
         else:
-            if gt_labels[gt_segments.index(gt_segment)] != gt_label:
-                gt_segments.append(gt_segment)
-                gt_labels.append(gt_label)
-            else:
-                continue
+            continue
 
     annotation = dict(
         gt_segments=np.array(gt_segments, dtype=np.float32),
@@ -34,3 +32,9 @@ if __name__ == "__main__":
     # output should be:
     # 'gt_segments': array([[3., 5.], [3., 6.], [3., 5.]], dtype=float32),
     # 'gt_labels': array([0, 1, 2], dtype=int32)}
+
+    anno3 = dict(gt_segments=np.array([[3, 5], [3, 5], [3, 5]]), gt_labels=np.array([0, 1, 1]))
+    print(filter_same_annotation(anno3))
+    # output should be:
+    # 'gt_segments': array([[3., 5.], [3., 5.]], dtype=float32),
+    # 'gt_labels': array([0, 1], dtype=int32)}
